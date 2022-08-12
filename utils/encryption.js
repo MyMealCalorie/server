5c435c83625b67e9ee4b207b669781aa12286c2b
chancho0518 <46704032+chancho0518@users.noreply.github.com>
2022-08-12 14:35:28 +0900

    feat: user 정보 암호화 및 정규 표현식 적용

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,1 +1,39 @@
 'use strict';
+
+// modules
+// crypto
+// 22.08.12 by Steve / crypto 이용한 코드 주석처리(추후 삭제 예정)
+// const crypto = require('crypto');
+// const genSalt = 'secret123';
+
+// const encodedPassword = (password) => {
+//     const encodedValue = crypto.createHash('sha512').update(password + genSalt).digest('base64');
+//     return password;
+// }
+
+// modules
+// bcrypt
+// 22.08.12 by Steve / bcrypt 적용한 코드 적용
+const bcrypt = require('bcrypt');
+
+const encryption = {
+    salt: () => {
+        const saltRounds = 10;
+        // 22.08.12 by Steve / bcrypt genSaltSync 매소드로 salt값 생성
+        // const genSalt = '$2b$10$PDOhXzZ2UYVN5FUiXLqqLu';
+        const genSalt = bcrypt.genSaltSync(saltRounds);
+        return genSalt;
+    },
+    hash: (value) => {
+        const hashValue = bcrypt.hashSync(value, encryption.salt());
+        return hashValue;
+        // 22.08.12 by Steve / bcrypt 미적용시 value return
+        // return value;
+    },
+    match: (value, hashedValue) => {
+        const compare = bcrypt.compareSync(value, hashedValue);
+        return compare;
+    }
+}
+
+module.exports = encryption;
--- utils/regexp.js
+++ utils/regexp.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// 22.08.12 by Steve / To-do: 정규 표현식은 client의 정규식과 확인 필요
+const regExp = {
+    isEmail: (asValue) => {
+        const re = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;;
+        return re.test(asValue);
+    },
+    isPassword: (asValue) => {
+        const re = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+        return re.test(asValue);
+    },
+};
+
+module.exports = regExp;
